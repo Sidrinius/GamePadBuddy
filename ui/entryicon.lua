ba950fb37537ac59a7d1448da05e7d026c536d47
Sidrinius <6269482+Sidrinius@users.noreply.github.com>
2020-07-13 19:37:37 -0500

    Fix Skill Menu Lockup
    Fix lockup upon entering skills menu.

--- ui/entryicon.lua
+++ ui/entryicon.lua
@@ -164,13 +164,13 @@ function OnOpenStore()
 end
 
 function HookDestructionList() 
-	local testfunction = _G.ZO_SharedSmithingExtraction_IsExtractableOrRefinableItem
-	_G.ZO_SharedSmithingExtraction_IsExtractableOrRefinableItem = function (bagId, slotIndex) 
+	local testfunction = _G.ZO_SharedSmithingExtraction_IsExtractableItem
+	_G.ZO_SharedSmithingExtraction_IsExtractableItem = function (itemData) 
 		if GamePadBuddy.curSavedVars.hideresearchables then
-			local isResearchItem = GamePadBuddy:GetItemFlagStatus(bagId, slotIndex) == GamePadBuddy.CONST.ItemFlags.ITEM_FLAG_TRAIT_RESEARABLE
-			return testfunction(bagId, slotIndex) and not isResearchItem
+			local isResearchItem = GamePadBuddy:GetItemFlagStatus(itemData.bagId, itemData.slotIndex) == GamePadBuddy.CONST.ItemFlags.ITEM_FLAG_TRAIT_RESEARABLE
+			return testfunction(itemData) and not isResearchItem
 		else
-			return testfunction(bagId, slotIndex)
+			return testfunction(itemData)
 		end
 	end
 end
